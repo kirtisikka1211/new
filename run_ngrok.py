3001aac61a1f0852eb1e6777a15c8f0a120874b8
kirtisikka <kirtisikka972@gmail.com>
2024-08-29 06:47:40 +0530

    first commit

--- run_ngrok.py
+++ run_ngrok.py
@@ -5,7 +5,7 @@ import os
 ngrok.set_auth_token("2lIKl9q7oYG4hMrktMGvB7gAlBL_2YCXjHuzv5cfZapkRwxu7")
 
 # Start ngrok to tunnel the Streamlit port
-port = 8505
+port = 8000
 public_url = ngrok.connect(port)
 print(f"Public URL: {public_url}")
 
